282f33fd8513c8efb2c5a559625a5369263437cc
Ramya <ramya.r@nativewit.in>
2025-07-30 15:18:17 +0530

    written more views with actions - index and detail page

--- polls/views.py
+++ polls/views.py
@@ -1,12 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from django.http import HttpResponse
-
+from .models import Question
+from django.template import loader
 
 #def index(request):
 #    return HttpResponse("Hello, world. You're at the polls index.")
-def detail(request,question_id):
-    return HttpResponse("You are look at question %s." % question_id)
+#def detail(request,question_id):
+#    return HttpResponse("You are look at question %s." % question_id)
 def results(request,question_id):
     response = "You are looking at results of question %s."
     return HttpResponse(response % question_id)
@@ -16,13 +17,39 @@ def vote(request,question_id):
     
 #    index page
 #    list recent top 3 question in index page
-def index(request):
-    latest_question_list = Question.objects.order_by("pub_date")[:3]
+#def index(request):
+#    latest_question_list = Question.objects.order_by("pub_date")[:3]
 #    output = ", ".join([q.question_text for q in latest_question_list])
 #    return HttpResponse(output)
-    template = loader.get_template("polls/index.html")
+
+#render template
+#def index(request):
+#    latest_question_list = Question.objects.order_by("pub_date")[:3]
+#    template = loader.get_template("polls/index.html")
+#    context = {"latest_question_list": latest_question_list}
+#    return HttpResponse(template.render(context, request))
+
+#render shortcut
+def index(request):
+    latest_question_list = Question.objects.order_by("pub_date")[:3]
     context = {"latest_question_list": latest_question_list}
-    return HttpResponse(template.render(context, request))
+    return render(request, "polls/index.html", context)
+#def detail(request,question_id):
+#    try:
+#       question = Question.objects.get(pk=question_id)
+#    except Question.DoesNotExist:
+#       raise Http404("Question does not exist")
+#       context = {"question": question}
+#    return render(request, "polls/detail.html", context)
+
+def detail(request,question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    context = {"question": question}
+    return render(request, "polls/detail.html", context)
+    
+
+
+    
     
  
 
