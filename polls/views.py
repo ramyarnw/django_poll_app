db4e40fdc2847363b66eb042776fa2e9128e393f
Ramya <ramya.r@nativewit.in>
2025-07-31 17:24:53 +0530

    minimal form and generic view

--- polls/views.py
+++ polls/views.py
@@ -4,50 +4,43 @@ from django.http import HttpResponse, HttpResponseRedirect
 from .models import Question, Choice
 from django.template import loader
 from django.db.models import F
+from django.views import generic
 
-#def index(request):
-#    return HttpResponse("Hello, world. You're at the polls index.")
-#def detail(request,question_id):
-#    return HttpResponse("You are look at question %s." % question_id)
-#def results(request,question_id):
-#    response = "You are looking at results of question %s."
-#    return HttpResponse(response % question_id)
-def vote(request,question_id):
-    response = "You are voting on question %s."
-    return HttpResponse(response % question_id)
-    
-#    index page
-#    list recent top 3 question in index page
+
+
+
+
+
+##render shortcut
 #def index(request):
 #    latest_question_list = Question.objects.order_by("pub_date")[:3]
-#    output = ", ".join([q.question_text for q in latest_question_list])
-#    return HttpResponse(output)
-
-#render template
-#def index(request):
-#    latest_question_list = Question.objects.order_by("pub_date")[:3]
-#    template = loader.get_template("polls/index.html")
 #    context = {"latest_question_list": latest_question_list}
-#    return HttpResponse(template.render(context, request))
-
-#render shortcut
-def index(request):
-    latest_question_list = Question.objects.order_by("pub_date")[:3]
-    context = {"latest_question_list": latest_question_list}
-    return render(request, "polls/index.html", context)
-    
+#    return render(request, "polls/index.html", context)
+#    
+#
+#   
 #def detail(request,question_id):
-#    try:
-#       question = Question.objects.get(pk=question_id)
-#    except Question.DoesNotExist:
-#       raise Http404("Question does not exist")
-#       context = {"question": question}
+#    question = get_object_or_404(Question, pk=question_id)
+#    context = {"question": question}
 #    return render(request, "polls/detail.html", context)
-   
-def detail(request,question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    context = {"question": question}
-    return render(request, "polls/detail.html", context)
+
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        """Return the last three published questions."""
+        return Question.objects.order_by("-pub_date")[:3]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -66,10 +59,10 @@ def vote(request, question_id):
 #        prevent data process twice, if we cick back
         return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
         
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    context = {"question": question}
-    return render(request, "polls/results.html", context)
+#def results(request, question_id):
+#    question = get_object_or_404(Question, pk=question_id)
+#    context = {"question": question}
+#    return render(request, "polls/results.html", context)
         
     
     
